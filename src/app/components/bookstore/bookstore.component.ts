72964390bb51b23f103dc0be7012ff0f7bdcb10a
jamersonmss <jamersonmssilva@gmail.com>
2022-08-06 12:02:29 -0300

    Projeto de criação de um front-end de um  e-commerce com Angular

---
+++ src/app/components/bookstore/bookstore.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookstore',
+  templateUrl: './bookstore.component.html',
+  styleUrls: ['./bookstore.component.css']
+})
+export class BookCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
